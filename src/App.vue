c477230938158efb1668ac520c38f11501fbda0f
Shawn Lee <changoloco.evg@gmail.com>
2022-05-03 07:41:42 -0500

    :tada: feat: initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <!-- title -->
+  Build The Community Your Fans Will Love
+
+  <!-- paragraph -->
+  Huddle re-imagines the way we build communities. You have a voice, but so does your audience.
+  Create connections with your users as you engage in genuine discussion.
+
+  <!-- button -->
+  Register
+</template>
